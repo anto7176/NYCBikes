ea7e93ae9ad787da2b76f4bc1274e0e8c7d5cc77
anto7176 <antoine.vadot71@gmail.com>
2026-02-25 14:26:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-mongodb+srv://<anto>:<ynHHkfVPz9AcSjR8>@nycbikes.2usntce.mongodb.net/?appName=NYCBikes
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+client = MongoClient("mongodb+srv://<anto>:<ynHHkfVPz9AcSjR8>@nycbikes.2usntce.mongodb.net/?appName=NYCBikes", server_api=ServerApi('1'))
+
+print(client)
