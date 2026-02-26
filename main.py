949b26bad8f580f7fda8af7c566f497ba85abfa8
anto7176 <antoine.vadot71@gmail.com>
2026-02-26 08:17:15 +0100

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
-
-client = MongoClient("mongodb+srv://<anto>:<ynHHkfVPz9AcSjR8>@nycbikes.2usntce.mongodb.net/?appName=NYCBikes", server_api=ServerApi('1'))
-
-print(client)
