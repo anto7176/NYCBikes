6a3bc04942a6b206af069bb6beff844b694f7ba2
floalzaix <floalzfencing@gmail.com>
2026-03-11 15:21:45 +0100

    [Front][Tested] Added the component for the charts

--- frontend/src/app/components/pages/home/home.ts
+++ frontend/src/app/components/pages/home/home.ts
@@ -6,6 +6,7 @@ import { HeatmapPoint } from '../../../models/heatmap-point';
 import { DatePicker } from 'primeng/datepicker';
 import { FormsModule } from '@angular/forms';
 import { FloatLabelModule } from 'primeng/floatlabel';
+import { Chart } from '../../chart/chart';
 
 @Component({
   selector: 'app-home',
@@ -14,7 +15,8 @@ import { FloatLabelModule } from 'primeng/floatlabel';
     UploadZone,
     DatePicker,
     FormsModule,
-    FloatLabelModule
+    FloatLabelModule,
+    Chart
   ],
   templateUrl: './home.html',
   styleUrl: './home.css',
